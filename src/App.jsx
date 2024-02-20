c2834810f3371bca39db87b5f8443eba1e3de46d
ZMP <zane.prunella@gmail.com>
2024-02-19 16:01:53 -0800

    linking context to state

--- src/App.jsx
+++ src/App.jsx
@@ -66,8 +66,13 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
-    <CartContext.Provider value={{ items: [] }}>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
